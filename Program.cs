3fa9d365824595fb54aa17fe478fa6bb02ba5311
brock810 <130688727+brock810@users.noreply.github.com>
2023-04-20 19:52:34 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace C_syntax
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response);
+        }
+    }
+
+}
